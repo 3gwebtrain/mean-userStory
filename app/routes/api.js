cd498f56ce5a71b9ca84fa2ffd776932c7383ff7
3gwebtrain <3gwebtrain@gmail.com>
2016-07-19 18:41:55 +0530

    now the story can post, and get

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,4 +1,5 @@
 var User = require('../models/user');
+var Story = require('../models/story');
 var config = require('../../config');
 
 var secretKey = config.secretKey;
@@ -9,7 +10,7 @@ var jsonwebtoken = require("jsonwebtoken");
 function createToken ( user ) {
 
 	var token = jsonwebtoken.sign({
-		_id : user.id,
+		id : user.id,
 		name : user.name,
 		username: user.username
 	}, secretKey, {
@@ -119,11 +120,38 @@ module.exports = function( app, express ) {
 
 	//Destination B
 
-	api.get("/", function( req, res ) {
+	api.route('/')
+		.post(function( req, res ) {
 
-		res.json("Hello World!")
+			var story = new Story({
+				creator : req.decoded.id,
+				content : req.body.content
+			})
 
-	})
+			story.save(function(err) {
+				if(err) {
+					res.send( err );
+					return;
+				}
+
+				res.json({message:"New Story Created!"})
+			})
+
+		})
+
+		.get(function( req, res ) {
+			
+			Story.find({ creator : req.decoded.id }, function( err, stories ) {
+
+				if( err ) {
+					res.send( err );
+					return;
+				}
+
+				res.json( stories );
+
+			})
+		})
 
 	return api;
 
